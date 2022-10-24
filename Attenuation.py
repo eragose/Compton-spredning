b39ef9e2c7d28a1c90c02d560a4c204cb521b50f
eragose <erik.o.linder@gmail.com>
2022-10-24 17:27:07 +0200

    FIltrering næsten færdig

--- Attenuation.py
+++ Attenuation.py
@@ -25,9 +25,15 @@ for files in read_files:
     np_array_values.append(pdfile)
 
 print(np_array_values)
+def getData(name):
+    return np.loadtxt("Attenuation/Attenuation Aluminium " + name + "_ch001.txt", skiprows=4)
 
-def getCounts(name: str, lc: int = 1, hc: int = 1000):
-    data = np.loadtxt("Attenuation/Attenuation Aluminium " + name + "_ch001.txt", skiprows=4)
+def getTime(data):
+    times = data[:,0]
+    start = times[0]
+    end = times[-1]
+    return (end-start)/10**8
+def getCounts(data, name: str, lc: int = 1, hc: int = 1000):
     counts = data[:, 1]
     (x, y) = np.unique(counts, return_counts=True)
     lI = np.where(x == lc)[0][0]
@@ -44,10 +50,13 @@ def gaussFit(x, mu, sig, a, b, c):
     return np.exp(lny) - (b*x+c)
 
 def getChannel(name: str, data: tuple, lower_limit: int, upper_limit: int, guess: [int, int, int]):
-    x = data[0][lower_limit:upper_limit]
-    y = data[1][lower_limit:upper_limit]
+    ll = np.where(data[0] > lower_limit)[0][0]
+    ul = np.where(data[0] < upper_limit)[0][-1]
+    x = data[0][ll:ul]
+    y = data[1][ll:ul]
     yler = np.sqrt(y)
-    pinit = guess + [0,0]
+    pinit = guess + [30,-5]
+    print(pinit)
     xhelp = np.linspace(lower_limit, upper_limit, 500)
     popt, pcov = curve_fit(gaussFit, x, y, p0=pinit, sigma=yler, absolute_sigma=True)
     print(name)
@@ -59,20 +68,31 @@ def getChannel(name: str, data: tuple, lower_limit: int, upper_limit: int, guess
     print('usikkerheder:', perr)
     chmin = np.sum(((y - gaussFit(x, *popt)) / yler) ** 2)
     print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
+    plt.plot(x, y, label="data")
+    plt.plot(xhelp, gaussFit(xhelp, *popt), label="fit")
+    plt.show()
 
 
 
     return [popt, perr]
 
-Back = getCounts("bg")
-A1 = getCounts("1")
-A2 = getCounts("2")
-A3 = getCounts("3")
-A4 = getCounts("4")
-A5 = getCounts("5")
-A6 = getCounts("6")
-A7 = getCounts("7")
-print(len(Back), len(A1))
+for i in np.linspace(0,7,8):
+    i = str(int(i))
+    data = getData(i)
+    time = getTime(data)
+    countData = getCounts(data, i)
+    fit = getChannel(i, countData, 500, 700, [640, 10, 10])
+
+
+#Back = getCounts("bg")
+#A1 = getCounts("1")
+#A2 = getCounts("2")
+#A3 = getCounts("3")
+#A4 = getCounts("4")
+#A5 = getCounts("5")
+#A6 = getCounts("6")
+#A7 = getCounts("7")
+#print(len(Back), len(A1))
 def dif(a,b):
     Newx0 = []
     Newx1 = []
@@ -90,39 +110,39 @@ def dif(a,b):
         i += 1
     return Newx0,Newx1
 #print("test: ", A1)
-Newx = dif(Back,A1)
+#Newx = dif(Back,A1)
 #print(Newx)
-plt.plot(A1[0][400:800], A1[1][400:800], label = "A1")
-plt.plot(Newx[0][400:800],Newx[1][400:800], label = "dif")
-plt.plot(Back[0][400:800], Back[1][400:800], label = "back")
-plt.title('1')
-plt.legend()
-plt.show()
+#plt.plot(A1[0][400:800], A1[1][400:800], label = "A1")
+#plt.plot(Newx[0][400:800],Newx[1][400:800], label = "dif")
+#plt.plot(Back[0][400:800], Back[1][400:800], label = "back")
+#plt.title('1')
+#plt.legend()
+#plt.show()
 
 
 def logFit(x, a, b, c, d):
     return a/np.log(b*x+c)+d
 
-x = Back[0][200:990]
-y = Back[1][200:990]
-yler = np.sqrt(y)
-pinit =  [0.1, 0.00002, 1, 0]
-xhelp = np.linspace(200, 990, 500)
-popt, pcov = curve_fit(logFit, x, y, p0=pinit, sigma=y, absolute_sigma= True)
-print("back fit")
-print('a :', popt[0])
-print('b :', popt[1])
-print('c :', popt[2])
-print('d :', popt[3])
+#x = Back[0][200:990]
+#y = Back[1][200:990]
+#yler = np.sqrt(y)
+#pinit =  [0.1, 0.00002, 1, 0]
+#xhelp = np.linspace(200, 990, 500)
+#popt, pcov = curve_fit(logFit, x, y, p0=pinit, sigma=y, absolute_sigma= True)
+#print("back fit")
+#rint('a :', popt[0])
+#print('b :', popt[1])
+#print('c :', popt[2])
+#print('d :', popt[3])
 
-perr = np.sqrt(np.diag(pcov))
-print('usikkerheder:', perr)
-chmin = np.sum(((y - logFit(x, *popt)) / yler) ** 2)
-print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
+#perr = np.sqrt(np.diag(pcov))
+#print('usikkerheder:', perr)
+#chmin = np.sum(((y - logFit(x, *popt)) / yler) ** 2)
+#print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
 
-plt.plot(x, logFit(x, *popt), label = "fit")
+#plt.plot(x, logFit(x, *popt), label = "fit")
 
-plt.plot(Back[0][200:990], Back[1][200:990], label = "back")
-plt.title('bg')
-plt.legend()
-plt.show()
+#plt.plot(Back[0][200:990], Back[1][200:990], label = "back")
+#plt.title('bg')
+#plt.legend()
+#plt.show()
--- Vinkler.py
+++ Vinkler.py
@@ -9,19 +9,19 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 def get_time_counts(angle):
-    data_0 = np.loadtxt("Co\Comptorn spredning "+ angle + "_ch000.txt", skiprows=4)
-    data_1 = np.loadtxt("Co\Comptorn spredning "+ angle + "_ch001.txt", skiprows=4)
+    data_0 = np.loadtxt("Co\Comptorn spredning "+ str(angle) + "_ch000.txt", skiprows=4)
+    data_1 = np.loadtxt("Co\Comptorn spredning "+ str(angle) + "_ch001.txt", skiprows=4)
     #data fra 000
     time_in_02us = data_0[:,0]
     count_channel_0 = data_0[:,1]
     #data fra 001
     count_channel_1 = data_1[:,1]
 
-    time = (time_in_02us[-1] - time_in_02us[0])*5/1000000
+    time = (time_in_02us[-1] - time_in_02us[0])
     counts_0 = np.unique(count_channel_0[count_channel_0 > -100], return_counts=True)
     counts_1 = np.unique(count_channel_1[count_channel_1 > -100], return_counts=True)
 
     
     return time, counts_0, counts_1
-
-
+dat = get_time_counts(116)
+print(dat[0])
