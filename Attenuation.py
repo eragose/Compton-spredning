6e7dde2e1fed9e9381fe5304b378a6691cb16486
eragose <erik.o.linder@gmail.com>
2022-10-25 14:24:40 +0200

    FIltrering næsten færdig

--- Attenuation.py
+++ Attenuation.py
@@ -70,20 +70,28 @@ def getChannel(name: str, data: tuple, lower_limit: int, upper_limit: int, guess
     print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
     plt.plot(x, y, label="data")
     plt.plot(xhelp, gaussFit(xhelp, *popt), label="fit")
+    plt.legend()
+    plt.title(name)
     plt.show()
 
 
 
     return [popt, perr]
 
+intensities = []
 for i in np.linspace(0,7,8):
     i = str(int(i))
     data = getData(i)
     time = getTime(data)
     countData = getCounts(data, i)
     fit = getChannel(i, countData, 500, 700, [640, 10, 10])
-
-
+    area = fit[0][1]*fit[0][2]*2*np.pi
+    areaUncertaintyA = np.sqrt((fit[1][1]/fit[0][1])+(fit[1][2]/fit[0][2]))*2*np.pi
+    areaUncertainty = areaUncertaintyA/area
+    intensity = area/time
+    intensityUncertainty = areaUncertainty/time
+    intensities += [[intensity, intensityUncertainty]]
+intensities = np.array(intensities)
 #Back = getCounts("bg")
 #A1 = getCounts("1")
 #A2 = getCounts("2")
@@ -109,40 +117,35 @@ def dif(a,b):
             j += 1
         i += 1
     return Newx0,Newx1
-#print("test: ", A1)
-#Newx = dif(Back,A1)
-#print(Newx)
-#plt.plot(A1[0][400:800], A1[1][400:800], label = "A1")
-#plt.plot(Newx[0][400:800],Newx[1][400:800], label = "dif")
-#plt.plot(Back[0][400:800], Back[1][400:800], label = "back")
-#plt.title('1')
-#plt.legend()
-#plt.show()
+
 
 
 def logFit(x, a, b, c, d):
     return a/np.log(b*x+c)+d
 
-#x = Back[0][200:990]
-#y = Back[1][200:990]
-#yler = np.sqrt(y)
-#pinit =  [0.1, 0.00002, 1, 0]
-#xhelp = np.linspace(200, 990, 500)
-#popt, pcov = curve_fit(logFit, x, y, p0=pinit, sigma=y, absolute_sigma= True)
-#print("back fit")
-#rint('a :', popt[0])
-#print('b :', popt[1])
-#print('c :', popt[2])
-#print('d :', popt[3])
+def expFit(x, a, b, c):
+    return a*np.exp(x*b)+c
 
-#perr = np.sqrt(np.diag(pcov))
-#print('usikkerheder:', perr)
-#chmin = np.sum(((y - logFit(x, *popt)) / yler) ** 2)
-#print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
+x = np.linspace(0,7,8)*10.09
+xler = np.append([0], np.linspace(1,7,7)**0*0.02)
+y = intensities[:,0]
+yler = intensities[:,1]
+pinit =  [850, -0.05, 200]
+xhelp = np.linspace(0, 100, 100)
+popt, pcov = curve_fit(expFit, x, y, p0=pinit, sigma=yler, absolute_sigma= True)
+print("intensity fit")
+print('a :', popt[0])
+print('b :', popt[1])
+print('c :', popt[2])
 
-#plt.plot(x, logFit(x, *popt), label = "fit")
+perr = np.sqrt(np.diag(pcov))
+print('usikkerheder:', perr)
+chmin = np.sum(((y - expFit(x, *popt)) / yler) ** 2)
+print('chi2:', chmin, ' ---> p:', ss.chi2.cdf(chmin, 4))
 
-#plt.plot(Back[0][200:990], Back[1][200:990], label = "back")
-#plt.title('bg')
-#plt.legend()
-#plt.show()
+plt.plot(x, expFit(x, *popt), label = "fit")
+
+plt.errorbar(x, y, yerr = yler, xerr= xler, fmt='o', label = "Intensity")
+plt.title('Intesity')
+plt.legend()
+plt.show()
